c323343bb933e74f5b35d4374ce3f41b2b1b76f0
Josh Lohrman <joshlohrman@gmail.com>
2013-12-05 00:41:18 +0000

    moved example outside of project

--- example.js
+++
@@ -1,8 +0,0 @@
-var pokemon = require('./index.js');
-var fs      = require('fs');
-
-var skittypkx = fs.readFileSync('skitty_unencrypted.pkx');
-var skitty = Object.create(pokemon.PKX);
-skitty.load(skittypkx);
-process.stdout.write(skitty.pkxEncrypted);
-
